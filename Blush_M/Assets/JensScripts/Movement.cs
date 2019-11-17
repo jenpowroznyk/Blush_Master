77fac28a0a56cc211ea12f6d74080c08744cc695
jenpowroznyk <35268767+jenpowroznyk@users.noreply.github.com>
2019-11-17 17:39:51 -0500

    Movement Controller / Animations
    Anims are not toggled on at correct times

---
+++ Blush_M/Assets/JensScripts/Movement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+
+    Rigidbody rb;
+
+    public int speed;
+    public int jumpImpulse;
+
+    bool canJump = true;
+
+    void Start()
+    {
+        
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int xValue = 0;
+        int zValue = 0;
+        bool keyDown = false;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            zValue = 1;
+            keyDown = true;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            xValue = -1;
+            keyDown = true;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            zValue = -1;
+            keyDown = true;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            xValue = 1;
+            keyDown = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            Vector3 jumpVector = new Vector3(0, jumpImpulse, 0);
+            rb.velocity += jumpVector;
+            canJump = false;
+
+        }
+
+        if (keyDown)
+        {
+            //Vector3 velocity = new Vector3(xValue * speed * Time.deltaTime, 0, zValue * speed * Time.deltaTime);
+
+            //rb.AddForce(velocity, ForceMode.VelocityChange);
+
+
+            //Vector3 velocity = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+            //rb.velocity += velocity;
+
+        }
+
+        Vector3 velocity = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        rb.velocity += velocity;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
+}
---
+++ Blush_M/Assets/JensScripts/blending.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blending : MonoBehaviour
+{
+
+    private Animator anim;
+    private float h;
+    private float v;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        anim.SetFloat("Walk", h);
+       // anim.SetFloat("Turn", v);
+    }
+}
