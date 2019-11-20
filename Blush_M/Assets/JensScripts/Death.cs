794ccc782ff17b6615cb79af629162b38baf3b92
jenpowroznyk <35268767+jenpowroznyk@users.noreply.github.com>
2019-11-20 09:46:34 -0500

    Death / Water

---
+++ Blush_M/Assets/JensScripts/Death.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+   
+
+    public bool isDead;
+    private Transform charPos;
+
+    public Transform spawnP;
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDead = false;
+        charPos = this.GetComponent<Transform>();
+        //charPos = this.GetComponent<Transform>();
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Water")
+        {
+
+            isDead = true;
+            charPos.position = spawnP.position;
+            
+        }
+
+    }
+}
